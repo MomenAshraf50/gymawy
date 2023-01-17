62dfb2e3db2172ca2b648d6bad102cd1ad6ec5b2
mohammed omar <71285596+Mohammedomargallya39@users.noreply.github.com>
2023-01-17 12:08:03 +0200

    Add Exercise II

--- lib/core/util/widgets/progress.dart
+++ lib/core/util/widgets/progress.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:gymawy/core/util/resources/constants_manager.dart';
 import 'package:gymawy/core/util/resources/extensions_manager.dart';
-import 'package:gymawy/core/util/widgets/myText.dart';
-import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
 import '../resources/colors_manager.dart';
-
+import '../resources/constants_manager.dart';
+import 'myText.dart';
 
 class ProgressDialog extends Dialog {
-  final String progressAmount;
-  final double liquidValue;
-
+  final double height;
+  final double width;
+  final String message;
 
   const ProgressDialog({
-    required this.progressAmount,
-    required this.liquidValue,
-
+    this.height = 160.0,
+    this.width = 80.0,
+    required this.message,
     Key? key,
   }) : super(key: key);
 
@@ -25,23 +23,58 @@ class ProgressDialog extends Dialog {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
       //this right here
       child: SizedBox(
-        width: 13.w,
-        height: 6.h,
-        child: LiquidCircularProgressIndicator(
-          value: liquidValue,
-          valueColor:
-          const AlwaysStoppedAnimation(ColorsManager.mainColor),
-          backgroundColor: Colors.white,
-          borderColor: ColorsManager.mainColor,
-          borderWidth: 5.0,
-          direction: Axis.vertical,
-          center: myText(
-            title: progressAmount,
-            style: Style.extraSmall,
-            fontSize: 12.rSp,
+        height: height,
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              myText(
+                  title: message, align: TextAlign.center, style: Style.medium),
+              verticalSpace(5.h),
+              Container(
+                height: 1.h,
+                width: width,
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                      end: Alignment.bottomCenter,
+                      begin: Alignment.topCenter,
+                      colors:
+                      [
+                        Color.fromARGB(255, 255, 185, 145)  ,
+                        ColorsManager.mainColor
+                      ]
+                  ),
+                  borderRadius: BorderRadius.circular(10.rSp),
+                ),
+              ),
+              // myText(
+              //     title: message, align: TextAlign.center, style: Style.medium),
+              // const Spacer(),
+              // Row(
+              //   children: [
+              //     Expanded(
+              //       child: myButton(
+              //         onPressed: popButtonVoidCallback,
+              //         text: popButtonText,
+              //         fontSize: 20.rSp,
+              //       ),
+              //     ),
+              //     horizontalSpace(2.h),
+              //     Expanded(
+              //       child: myButton(
+              //         onPressed: pushButtonVoidCallback,
+              //         text: pushButtonText,
+              //         fontSize: 20.rSp,
+              //       ),
+              //     ),
+              //   ],
+              // ),
+            ],
           ),
         ),
-      )
+      ),
     );
   }
 }
